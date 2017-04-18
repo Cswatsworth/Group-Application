5fcea21474eedb1b0e4df485493021ec94a359df
Chad Swatsworth <ohio15state54@gmail.com>
2017-04-18 16:38:40 -0400

    added comments

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Group-Application
-
-
-#Hopefully this works
+I created a the password functions and add some css on to the index page for the submit buttons. Also added a mined minds picture to the layout. The functions work and are tested.
+EMAIL function will be used inside app.rb. 
--- functions.rb
+++ functions.rb
@@ -8,8 +8,9 @@ class Password
 	def password_requirments()
 		password[/[a-zA-Z0-9]{6,}/] == password
 	end
-
+	#password function. Can only contain letters, numbers and no less than 6 characters.
 	def password_match(confirm)
 		password.include?(confirm)
+	#password confirm function. When signing up making sure passwords match	
 	end
 end
