e24f7a70051488da65ca0cea1ccae6a278306f27
Chad Swatsworth <ohio15state54@gmail.com>
2017-04-18 20:46:17 -0400

    messed around with some functions again

--- app.rb
+++ app.rb
@@ -4,8 +4,15 @@ require 'sinatra'
 	enable :sessions
 
 	get '/' do
-		session[:email] = params[:email]
-		session[:password] = params[:password]
+		session[:backend_email] = params[:email]
+		session[:backend_password] = params[:password]
 		erb :index
 	end
 
+	get '/signup' do
+		erb :welcome
+	end
+
+	get '/signin' do
+	 	erb :continue
+	end
