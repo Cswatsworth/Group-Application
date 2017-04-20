036ce3f36015fcfe1ae37669442f9308eabb7a68
Chad Swatsworth <ohio15state54@gmail.com>
2017-04-20 17:02:55 -0400

    pull

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pg'
 require_relative 'functions.rb'
 
 load './local_env.rb' if File.exists?('./local_env.rb')
@@ -16,22 +17,22 @@ db = PG::Connection.new(db_params)
 	enable :sessions
 
 	get '/' do
-		session[:backend_email] = params[:email]
-		session[:backend_password] = params[:password]
+		# session[:backend_email] = params[:email]
+		# session[:backend_password] = params[:password]
 		mmapplication = db.exec("SELECT email, password FROM mmapplication");
 		erb :index, locals: {mmapplication: mmapplication}
 	end
 
-	post'/signup' do
-		session[:backend_1st_name] = params[:first]
-		session[:backend_lst_name] = params[:last]
-		session[:backend_street] = params[:street]
-		session[:backend_city] = params[:city]
-		session[:backend_state] = params[:state]
-		session[:backend_zip] = params[:zip]
-		erb :page1, :locals => {first: session[:backend_1st_name], last: session[:backend_lst_name], street: session[:backend_street], city: session[:backend_city], state: session[:backend_state], zip: session[:backend_zip]}
-	end
+	# post'/signup' do
+	# 	session[:backend_1st_name] = params[:first]
+	# 	session[:backend_lst_name] = params[:last]
+	# 	session[:backend_street] = params[:street]
+	# 	session[:backend_city] = params[:city]
+	# 	session[:backend_state] = params[:state]
+	# 	session[:backend_zip] = params[:zip]
+	# 	erb :page1, :locals => {first: session[:backend_1st_name], last: session[:backend_lst_name], street: session[:backend_street], city: session[:backend_city], state: session[:backend_state], zip: session[:backend_zip]}
+	# end
 
-	get '/signin' do
-	 	erb :signin
-	end
+	# get '/signin' do
+	#  	erb :signin
+	# end
